f5e74ba6707484b26941737204c5c6f0885adf7f
quaintdev <59229571+quaintdev@users.noreply.github.com>
2022-07-19 23:36:51 +0530

    removed hard coded home direcotry

--- install/setup.sh
+++ install/setup.sh
@@ -1,11 +1,11 @@
 echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
 
-cd /home/pi
+cd $HOME
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
 git clone https://github.com/ruundii/bthidhub
-cd /home/pi/bthidhub/install/on_rpi
+cd $HOME/bthidhub/install/on_rpi
 sh ./on_pi_setup.sh
